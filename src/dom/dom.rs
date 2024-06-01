6302cae54b7bc8fa742d3216505e77ed23cc894e
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-01 10:43:53 +0200

    feat: impl dom structre

--- src/dom/dom.rs
+++ src/dom/dom.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
 pub struct Node {
@@ -17,4 +17,25 @@ pub type AtributeMapType = HashMap<String, String>;
 pub struct Element {
   pub tag_name: String,
   pub atributes: AtributeMapType,
+}
+
+pub fn create_text_node(text: String) -> Node {
+  Node { children: Vec::new(), node_type: NodeType::Text(text) }
+}
+pub fn create_element_node(tag_name: String, atributes: AtributeMapType) -> Node {
+  Node { children: Vec::new(), node_type: NodeType::Element(Element { tag_name, atributes }) }
+}
+
+impl Element {
+  pub fn id(&self) -> String {
+    self.atributes.get("id").unwrap().to_string()
+  }
+
+  pub fn classes(&self) -> HashSet<String> {
+    let mut classes = HashSet::new();
+    for class in self.atributes.get("class").unwrap().split(" ") {
+      classes.insert(class.to_string());
+    }
+    classes
+  }
 }
