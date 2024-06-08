57c1e184c1049baa32c87da3d0612a10f91653fe
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-08 11:56:19 +0200

    *

--- src/utils/validation.rs
+++ src/utils/validation.rs
@@ -1,6 +1,7 @@
-pub fn is_valid_identifier_char(c: char) -> bool {
+#[allow(dead_code)]
+pub fn is_tag_char(c: char) -> bool {
   match c {
-    'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' => true, // TODO: Include U+00A0 and higher.
+    'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' => true,
     _ => false,
   }
 }
