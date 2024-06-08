384f5ee0c573aba30215090f02bce5a17ff17800
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-08 11:56:19 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use html::HTMLParser;
+
+#[allow(dead_code)]
 mod css;
 mod dom;
 mod html;
@@ -5,5 +8,15 @@ mod layout;
 mod paint;
 mod utils;
 fn main() {
-  println!("Hello, world!");
+  let html = r#"<html>
+    <body>
+      <h1>Title</h1>
+      <div id="main" class="test">
+        <p>Hello <em>world</em>!</p>
+      </div>
+    </body>
+  </html>"#;
+  let mut html = HTMLParser::new(html.to_string());
+  let node = html.parse();
+  println!("{:#?}", node);
 }
