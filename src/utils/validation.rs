4a1604125c9efa05e36c4904a24941413e9803a9
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-09 18:47:33 +0200

    *

--- src/utils/validation.rs
+++ src/utils/validation.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
-pub fn is_tag_char(c: char) -> bool {
-  match c {
+pub fn is_tag_char(character: char) -> bool {
+  // TODO: Include U+00A0 and higher.
+  match character {
     'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' => true,
     _ => false,
   }
