fbcb21fdd531fe6442e86c3fe17eaa74eb2cf535
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-08 23:34:56 +0200

    *

---
+++ src/diagnostics/diagnostics.rs
@@ -0,0 +1,29 @@
+enum DiagnosticEnum {
+  Warning,
+  Error,
+  Info,
+}
+
+pub struct Diagnostics {
+  pub errors: Vec<String>,
+  pub warnings: Vec<String>,
+  pub infos: Vec<String>,
+}
+
+impl Diagnostics {
+  pub fn new() -> Self {
+    Diagnostics { errors: Vec::new(), warnings: Vec::new(), infos: Vec::new() }
+  }
+
+  pub fn add_error(&mut self, message: String) {
+    self.errors.push(message);
+  }
+
+  pub fn add_warning(&mut self, message: String) {
+    self.warnings.push(message);
+  }
+
+  pub fn add_info(&mut self, message: String) {
+    self.infos.push(message);
+  }
+}
