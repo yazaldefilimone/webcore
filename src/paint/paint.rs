fa21b78f0508fccf60a2fcb8ae2843cd25f2967d
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-15 01:53:26 +0200

    *

--- src/paint/paint.rs
+++ src/paint/paint.rs
@@ -1,7 +1,8 @@
+#![allow(dead_code)]
 use super::canvas::Canvas;
 use crate::layout::LayoutBox;
 
-pub fn paint(canvas: &mut Canvas, layout_box: &LayoutBox) {
+pub fn paint(canvas: &mut Canvas, _layout_box: &LayoutBox) {
   let _context = &mut canvas.context;
   // context.clear_rect(0.0, 0.0, canvas.width, canvas.height);
   // context.fill_rect(0.0, 0.0, canvas.width, canvas.height);
