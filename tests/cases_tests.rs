9b540879cbd2215bee194c7db272881fbbd4f542
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-14 22:39:38 +0200

    *

--- tests/cases_tests.rs
+++ tests/cases_tests.rs
@@ -1,25 +1,29 @@
 use insta::assert_snapshot;
 use itertools::Itertools;
-use std::{
-  collections::HashMap,
-  path::{Path, PathBuf},
-};
+use std::{collections::HashMap, path::Path};
+use web_core::css::{CSSParser, StyleSheet};
 use web_core::dom::Node;
 use web_core::html::HTMLParser;
 
-const TESTS_PATH: &str = "tests/cases/";
+const CSS_TESTS_PATH: &str = "tests/cases/css/";
+const HTML_TESTS_PATH: &str = "tests/cases/html/";
 
-type RunFn = dyn Fn(&str) -> Node;
+enum TestResult {
+  Node(Node),
+  StyleSheet(StyleSheet),
+}
 
-fn run_test(name: &str, path: &Path, run: &[&RunFn]) {
+type RunFn = dyn Fn(&str) -> TestResult;
+
+fn run_test(name: &str, path: &Path, run: &[&RunFn], module_path: &str) {
   println!("Running test: {}...", name);
   println!("path: {}", path.display());
   let code = std::fs::read_to_string(path).expect("Failed to read test file");
-  let file_name = path.to_str().and_then(|path| path.rsplit_once(TESTS_PATH)).unwrap().1;
-  let file_path = format!("{}{}", &TESTS_PATH[1..], file_name);
+  let file_name = path.to_str().and_then(|path| path.rsplit_once(module_path)).unwrap().1;
+  let file_path = format!("{}{}", &module_path[1..], file_name);
   let file_path = Path::new(&file_path);
 
-  let mut results: HashMap<&Path, Vec<Node>> = HashMap::new();
+  let mut results: HashMap<&Path, Vec<TestResult>> = HashMap::new();
 
   for run_fn in run {
     let result = run_fn(&code);
@@ -28,7 +32,15 @@ fn run_test(name: &str, path: &Path, run: &[&RunFn]) {
 
   let results = results
     .into_values()
-    .map(|nodes| nodes.into_iter().map(|node| format!("{:#?}", node)).collect_vec())
+    .map(|results| {
+      results
+        .into_iter()
+        .map(|result| match result {
+          TestResult::Node(node) => format!("{:#?}", node),
+          TestResult::StyleSheet(stylesheet) => format!("{:#?}", stylesheet),
+        })
+        .collect_vec()
+    })
     .flatten()
     .collect_vec();
 
@@ -38,12 +50,12 @@ fn run_test(name: &str, path: &Path, run: &[&RunFn]) {
   settings.set_input_file(path);
   settings.bind(|| {
     for result in results {
-      assert_snapshot!(file_name, result);
+      assert_snapshot!(result);
     }
   });
 }
 
-fn run_tests(path: &Path, run: &[&RunFn]) {
+fn run_tests(path: &Path, run: &[&RunFn], module_path: &str) {
   let mut paths = Vec::new();
   for entry in std::fs::read_dir(path).unwrap() {
     let entry = entry.unwrap();
@@ -55,17 +67,22 @@ fn run_tests(path: &Path, run: &[&RunFn]) {
 
   paths.sort();
   for path in paths {
-    let name = path.to_str().unwrap().split(TESTS_PATH).last().unwrap();
-    run_test(name, &path, run);
+    let name = path.to_str().unwrap().split(module_path).last().unwrap();
+    run_test(name, &path, run, module_path);
   }
 }
 
-fn parse_html(code: &str) -> Node {
+fn parse_html(code: &str) -> TestResult {
   let mut parser = HTMLParser::new(code.to_string());
-  parser.parse()
+  TestResult::Node(parser.parse())
 }
 
-fn run_multiple_dirs(path: &Path, run: &[&RunFn]) {
+fn parse_css(code: &str) -> TestResult {
+  let mut parser = CSSParser::new(code.to_string());
+  TestResult::StyleSheet(parser.parse())
+}
+
+fn run_multiple_dirs(path: &Path, run: &[&RunFn], module_path: &str) {
   let root = path.to_str().unwrap();
   let walker = walkdir::WalkDir::new(path).into_iter();
   for entry in walker.filter_map(|e| e.ok()) {
@@ -73,13 +90,19 @@ fn run_multiple_dirs(path: &Path, run: &[&RunFn]) {
     if path.is_file() {
       let name = path.to_str().unwrap().split(root).last().unwrap();
       eprintln!("Running test: {}...", name);
-      run_test(name, &path, run);
+      run_test(name, &path, run, module_path);
     }
   }
 }
 
 #[test]
 fn test_html_parser() {
-  let path = Path::new(TESTS_PATH);
-  run_multiple_dirs(path, &[&parse_html]);
+  let path = Path::new(HTML_TESTS_PATH);
+  run_multiple_dirs(path, &[&parse_html], HTML_TESTS_PATH);
+}
+
+#[test]
+fn test_css_parser() {
+  let path = Path::new(CSS_TESTS_PATH);
+  run_multiple_dirs(path, &[&parse_css], CSS_TESTS_PATH);
 }
